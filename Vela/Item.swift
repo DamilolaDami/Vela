d5a075c3cff2a16cdf1ad1348c43d839e34faf9c
Dami <kabirabdul08091@gmail.com>
2025-05-30 02:54:37 +0100

    Initial Commit

---
+++ Vela/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Vela
+//
+//  Created by damilola on 5/30/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ VelaTests/VelaTests.swift
@@ -0,0 +1,17 @@
+//
+//  VelaTests.swift
+//  VelaTests
+//
+//  Created by damilola on 5/30/25.
+//
+
+import Testing
+@testable import Vela
+
+struct VelaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
